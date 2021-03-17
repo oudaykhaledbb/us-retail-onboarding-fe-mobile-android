4b85161ca82a5569bd8ff430e9d8cc63f7dccd23
Ouday Khaled <ouday@backbase.com>
2021-03-17 14:16:31 +0100

    - Add SMEO business

--- smeo-business/src/main/java/com/backbase/android/flow/smeo/business/ui/BusinessInfoScreen.kt
+++ smeo-business/src/main/java/com/backbase/android/flow/smeo/business/ui/BusinessInfoScreen.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.backbase.android.flow.smeo.business.R
+import kotlinx.android.synthetic.main.screen_business_info.*
 import org.koin.android.ext.android.inject
 
 class BusinessInfoScreen : Fragment(R.layout.screen_business_info) {
@@ -12,7 +13,8 @@ class BusinessInfoScreen : Fragment(R.layout.screen_business_info) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        txtInputKnownName.getChildAt(1).visibility = View.GONE
+        txtInputEin.getChildAt(1).visibility = View.GONE
     }
 
 }
