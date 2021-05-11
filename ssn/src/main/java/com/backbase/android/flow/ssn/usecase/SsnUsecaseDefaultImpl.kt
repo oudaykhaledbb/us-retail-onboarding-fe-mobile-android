80d67232c50eeddf40075c23a682ec0436b6ba12
Ouday Khaled <ouday@backbase.com>
2021-05-12 01:29:44 +0300

    Add Personal Address Validation

--- ssn/src/main/java/com/backbase/android/flow/ssn/usecase/SsnUsecaseDefaultImpl.kt
+++ ssn/src/main/java/com/backbase/android/flow/ssn/usecase/SsnUsecaseDefaultImpl.kt
@@ -5,7 +5,6 @@ import com.backbase.android.flow.common.interaction.performInteraction
 import com.backbase.android.flow.contracts.FlowClientContract
 import com.backbase.android.flow.ssn.SsnConfiguration
 import com.backbase.android.flow.ssn.models.SsnModel
-import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 
 private const val JOURNEY_NAME = "sme-onboarding-ssn"
@@ -17,7 +16,6 @@ class SsnUsecaseDefaultImpl(
 ) : SsnUsecase {
 
     override suspend fun submitSsn(ssn: String): Any? {
-        submitPersonalAddress()// TODO code to be removed when Submit Personal Address Journey implemented
         val result = performInteraction<SsnModel, Any?>(
             configuration.isOffline,
             context,
@@ -29,26 +27,6 @@ class SsnUsecaseDefaultImpl(
         )
         landing()
         return result
-    }
-
-    //TODO Remove below classes
-    suspend fun submitPersonalAddress(): Any? {
-        return performInteraction(
-            configuration.isOffline,
-            context,
-            JOURNEY_NAME,
-            flowClient,
-            object : TypeToken<Any?>() {}.type,
-            "submit-address",
-            PersonalAddressModel(
-                "17 E Jefferson St",
-                "41",
-                22222,
-                "Phoenix",
-                "AZ",
-                "Personal",
-            )
-        )
     }
 
     suspend fun landing(): Any? {
@@ -63,20 +41,3 @@ class SsnUsecaseDefaultImpl(
     }
 
 }
-
-//TODO Remove below classes
-data class PersonalAddressModel(
-    @SerializedName("numberAndStreet")
-    val numberAndStreet: String,
-    @SerializedName("apt")
-    val apt: String,
-    @SerializedName("zipCode")
-    val zipCode: Int,
-    @SerializedName("city")
-    val city: String,
-    @SerializedName("state")
-    val state: String,
-    @SerializedName("addressName")
-    val addressName: String
-)
-//end TODO Remove below classes
